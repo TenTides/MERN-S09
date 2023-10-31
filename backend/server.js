4b61773e47560106a38c9ec39c053fabc4ae45ec
Tyler <tyler001crawford@gmail.com>
2023-10-31 19:55:02 -0400

    MERN Backend Boilerplate

---
+++ backend/server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const employeeRoutes = require('./routes/employees')
+//Express App is started
+const app = express() 
+
+// Debugging Scripts Runs on every request, logging each request
+app.use(express.json()) // attaches json to requests
+app.use((req,res,next) =>{
+    console.log(req.path,req.method)
+    next()
+})
+
+//routes
+app.use('/api/employees',employeeRoutes)
+
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //Listen For requests on a given port
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to DB')
+            console.log('Listening on port',process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+//get request handler for get on /
+app.get('/', (req, res) => {
+    res.json({msg: "Get req detected successfuly"})
+})
