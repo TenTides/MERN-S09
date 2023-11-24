3a66a9babe6b2484688f8134c7b9a29927de5e1e
Tyler <tyler001crawford@gmail.com>
2023-11-24 16:08:55 -0500

    pathing

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ app.use(session({
 // Debugging Scripts Runs on every request, logging each request
 if(process.env.NODE_ENV == "production")
 {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
+    app.use(express.static(path.join(__dirname, '/client/build')))
     app.get('*',(req,res) =>{
-        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
     })
 }
 
