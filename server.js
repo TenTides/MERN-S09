320377fe5cbb4e75147ee9668f99dcf6c9cc31cc
Tyler <tyler001crawford@gmail.com>
2023-11-25 12:14:38 -0500

    cors

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const verifyEmailRoutes = require('./routes/verifyEmail');
 
 // Express App is started
 const app = express() 
+app.use(cors());
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(express.json({ limit: '10mb' })) // attaches json to requests
 // Session Declaration
