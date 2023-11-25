3d6c886c4c01f2c559f054bbbb22a60966ad035a
Tyler <tyler001crawford@gmail.com>
2023-11-25 15:59:01 -0500

    getuserSingle

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,11 +16,21 @@ const getUsers = async (req, res) => {
 
 const getUser = async (req, res) => {
     const { id } = req.params;
+
     try {
-        const user = await User.findById(id); // Fetch all users
+        const user = await User.findById(id);
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        // remove sensitive information before sending the user data
+        //user.password = undefined;
+        //user.verificationToken = undefined;
+
         res.status(200).json(user);
     } catch (error) {
-        res.status(500).json({ message: 'Error fetching users', error: error.message });
+        res.status(500).json({ message: 'Error fetching user', error: error.message });
     }
 };
 
