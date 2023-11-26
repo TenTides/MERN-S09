0d93819cdabe474324ea21d30c483a2fb7af504c
Tyler <tyler001crawford@gmail.com>
2023-11-26 13:55:39 -0500

    full integration

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 // import Navbar from "./components/navbar";
 import Images from './pages/Images';
 import {Login, Register} from './pages/Login';
+import {SignInSignUpForm} from './pages/account.js';
+
 import Verify from './pages/Verify';
 
 
@@ -12,6 +14,7 @@ function App() {
 			<Routes>
 				<Route path="/register" element={<Register/>}></Route>
 				<Route path="/login" element={<Login/>}></Route>
+				<Route path="/signinsignup" element={<SignInSignUpForm/>}></Route>
 				<Route path="/verify" element={<Verify/>}></Route>
 				<Route path="/images" element={<Images />} />
 				<Route path="/" element={<Home/>}>
