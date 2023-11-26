f2991d5727e2c629edd32ff1b8e6225012e4d1f0
Tyler <tyler001crawford@gmail.com>
2023-11-25 20:30:58 -0500

    undo

--- client/src/pages/Verify.js
+++ client/src/pages/Verify.js
@@ -32,9 +32,6 @@ const Verify = () => {
 
         const json = await response.json();
         setMessage(json.message);
-        setTimeout(() => {
-          history.push('http://poosd.com');
-        }, 5000);
       } catch (error) {
         console.log('Token:', token);
         console.error('Error fetching data:', error.message);
