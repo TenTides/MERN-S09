d3b0da3cb43e741d0eeae6f426ea538a4a5a47b1
Garrett Colon <82895366+gco1on@users.noreply.github.com>
2023-11-25 22:34:49 -0500

    Update Login.js UI

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -37,17 +37,78 @@ const Login = () => {
         }
     };
 
-    return (
-        <div className='forms'>
-            <form className='login' onSubmit={handleLoginSubmit}>
-                <input type="email" value={loginEmail} onChange={e => setLoginEmail(e.target.value)} placeholder="Login email" />
-                <input type="password" value={loginPassword} onChange={e => setLoginPassword(e.target.value)} placeholder="Login password" />
-                <button type="submit">Login</button>
-            </form>
+    const signin_btn = document.querySelector("#signin-btn");
+    const signup_btn = document.querySelector("#signup-btn");
+    const container = document.querySelector(".container");
+
+    signup_btn.addEventListener('click', () => {
+        container.classList.add("signup-mode");
+    });
+
+    signin_btn.addEventListener('click', () => {
+        container.classList.remove("signup-mode");
+    });
+
+    return(
+    <><head>
+        <title>Sign In & Sign Up Form</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="forms-container">
+                <div class="signin-signup">
+                    <form action="" class="signin-form">
+                        <h2 class="title">Sign in</h2>
+                        <div class="input-field">
+                            <i class="fas fa-envelope"></i>
+                            <input type="email" placeholder="Email" />
+                        </div>
+                        <div class="input-field">
+                            <i class="fas fa-lock"></i>
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <input type="submit" value="Login" class="btn solid" />
+                    </form>
+
+                    <form action="" class="signup-form">
+                        <h2 class="title">Sign up</h2>
+                        <div class="input-field">
+                            <i class="fas fa-envelope"></i>
+                            <input type="email" placeholder="Email" />
+                        </div>
+                        <div class="input-field">
+                            <i class="fas fa-lock"></i>
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <input type="submit" value="Sign up" class="btn solid" />
+                    </form>
+                </div>
+            </div>
+
+            <div class="panels-container">
+                <div class="panel left-panel">
+                    <div class="content">
+                        <h3>New here?</h3>
+                        <p>Create an account and start using #Photo4U</p>
+                        <button class="btn transparent" id="signup-btn">Sign up</button>
+                    </div>
+
+                    <img src="client/src/pages/img/log.svg" class="image" alt="" />
+                </div>
+
+                <div class="panel right-panel">
+                    <div class="content">
+                        <h3>Already a member?</h3>
+                        <p>Login and welcome back to #Photo4U</p>
+                        <button class="btn transparent" id="signin-btn">Sign in</button>
+                    </div>
+
+                    <img src="client/src/pages/img/reg.svg" class="image" alt="" />
+                </div>
+            </div>
         </div>
+    </body></>    
     );
 };
 
 export default Login;
-
-
